1e520eba2ec32cd8ec9ccc5c2de071bdd17209fd
TuAnhTo <46498571+TuAnhTo@users.noreply.github.com>
2019-01-10 11:54:26 +0700

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,6 @@
+var  witdth;
+var height;
+    witdth = parseInt(prompt(" nhap chieu cao"));
+    height = parseInt(prompt(" nhap chieu rong"));
+var S = witdth * height;
+    document.write("dien tich la " +S)
